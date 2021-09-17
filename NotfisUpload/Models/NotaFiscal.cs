d1c1d2b9c46a6e8829b831df914d722b5bd4d667
Weslei Correa <wesleiwc7@gmail.com>
2021-09-17 11:22:16 -0300

    initial commit

---
+++ NotfisUpload/Models/NotaFiscal.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NotfisUpload.Models
+{
+    public class NotaFiscal
+    {
+        public string ID { get; set; }
+        public string ID_intercambio { get; set; }
+        public string RazaoSocial { get; set; }
+    }
+}
